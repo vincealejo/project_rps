a3259a70488933067d5b16000b2d4978e2c24428
vince <219931481+vincealejo@users.noreply.github.com>
2025-07-23 15:19:53 +0800

    Add index.html and external javascript
    Finish getComputerChoice function
    It gets a random number from 0 - 2 and each number
    have a coresponding hand (rock, paper or scissors)

---
+++ script.js
@@ -0,0 +1,14 @@
+let computerScore = 0;
+let humanSCore = 0;
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if(randomNumber === 0) {
+        return "rock";
+    } else if(randomNumber === 1) {
+        return "paper";
+    } else if(randomNumber === 2) {
+        return "scissors";
+    }
+}
+
